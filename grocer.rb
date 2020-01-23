1e954c5d31d7883f99e2f9a1bf6499ef91113ba9
calm-fork-7535 <komald.4@gmail.com>
2020-01-23 18:34:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,6 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   counter = 0
   while counter < collection.length #3
     if collection[counter][:item] == name
+      return collection[counter]
 
     end
     #collection[counter]
