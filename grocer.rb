c3794f5ad6c8e409a3023aa38d60ad3e16c83140
calm-fork-7535 <komald.4@gmail.com>
2020-01-23 18:30:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,11 @@
-require 'pry' #pry is some type of playground.
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-
+  counter = 0
+  while counter < collection.length
+    collection[counter]
+  end
 end
 
 def consolidate_cart(cart)
@@ -11,7 +13,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-    binding.pry
+
 end
 
 def apply_coupons(cart, coupons)
