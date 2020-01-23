71fd7ca22052469cccae05fa2ff7b083e0ecd04a
calm-fork-7535 <komald.4@gmail.com>
2020-01-23 19:36:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,9 +36,10 @@ end
 def apply_coupons(cart, coupons)
   counter = 0
   while counter < coupons.length
-
-counter += 1
-  end
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    
+    counter += 1
+ end
 end
 
 def apply_clearance(cart)
